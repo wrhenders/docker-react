398f8c84e5cdd360ff1199642b1d17611736f565
Ryan Henderson <wrhenders@gmail.com>
2022-07-27 18:00:41 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Trial Elevensies</p>
+        <p>Last Call</p>
         <a
           className="App-link"
           href="https://reactjs.org"
